af0f859f211eec2fcbfdf0cfe2896738d8056d5f
Heitor Belloni <heitor.belloni@gmail.com>
2014-07-10 17:44:14 -0300

    Lazy evaluate removed. Set args on delegate fixed.

--- Evaluant.Calculator/Domain/EvaluationVisitor.cs
+++ Evaluant.Calculator/Domain/EvaluationVisitor.cs
@@ -91,34 +91,18 @@ namespace NCalc.Domain
 
         public override void Visit(BinaryExpression expression)
         {
-            // simulate Lazy<Func<>> behavior for late evaluation
-            object leftValue = null;
-            Func<object> left = () =>
-                                 {
-                                     if (leftValue == null)
-                                     {
-                                         expression.LeftExpression.Accept(this);
-                                         leftValue = Result;
-                                     }
-                                     return leftValue;
-                                 };
-
-            // simulate Lazy<Func<>> behavior for late evaluation
-            object rightValue = null;
-            Func<object> right = () =>
-            {
-                if (rightValue == null)
-                {
-                    expression.RightExpression.Accept(this);
-                    rightValue = Result;
-                }
-                return rightValue;
-            };
+            // Evaluates the left expression and saves the value
+            expression.LeftExpression.Accept(this);
+            object left = Result;
+            
+            // Evaluates the right expression and saves the value
+            expression.RightExpression.Accept(this);
+            object right = Result;
 
             // Calls external implementation
             BinaryExpressionArgs args = new BinaryExpressionArgs();
-            args.LeftValue = left();
-            args.RightValue = right();
+            args.LeftValue = left;
+            args.RightValue = right;
             OnEvaluateBinaryExpression(expression.Type, args);
 
             // If an external implementation was found get the result back
@@ -126,99 +110,101 @@ namespace NCalc.Domain
                 Result = args.Result;
                 return;
             }
+            left = args.LeftValue;
+            right = args.RightValue;
 
             switch (expression.Type)
             {
                 case BinaryExpressionType.And:
-                    Result = Convert.ToBoolean(left()) && Convert.ToBoolean(right());
+                    Result = Convert.ToBoolean(left) && Convert.ToBoolean(right);
                     break;
 
                 case BinaryExpressionType.Or:
-                    Result = Convert.ToBoolean(left()) || Convert.ToBoolean(right());
+                    Result = Convert.ToBoolean(left) || Convert.ToBoolean(right);
                     break;
 
                 case BinaryExpressionType.Div:
-                    Result = IsReal(left()) || IsReal(right())
-                                 ? Numbers.Divide(left(), right())
-                                 : Numbers.Divide(Convert.ToDouble(left()), right());
+                    Result = IsReal(left) || IsReal(right)
+                                 ? Numbers.Divide(left, right)
+                                 : Numbers.Divide(Convert.ToDouble(left), right);
                     break;
 
                 case BinaryExpressionType.Equal:
                     // Use the type of the left operand to make the comparison
-                    Result = CompareUsingMostPreciseType(left(), right()) == 0;
+                    Result = CompareUsingMostPreciseType(left, right) == 0;
                     break;
 
                 case BinaryExpressionType.Greater:
                     // Use the type of the left operand to make the comparison
-                    Result = CompareUsingMostPreciseType(left(), right()) > 0;
+                    Result = CompareUsingMostPreciseType(left, right) > 0;
                     break;
 
                 case BinaryExpressionType.GreaterOrEqual:
                     // Use the type of the left operand to make the comparison
-                    Result = CompareUsingMostPreciseType(left(), right()) >= 0;
+                    Result = CompareUsingMostPreciseType(left, right) >= 0;
                     break;
 
                 case BinaryExpressionType.Lesser:
                     // Use the type of the left operand to make the comparison
-                    Result = CompareUsingMostPreciseType(left(), right()) < 0;
+                    Result = CompareUsingMostPreciseType(left, right) < 0;
                     break;
 
                 case BinaryExpressionType.LesserOrEqual:
                     // Use the type of the left operand to make the comparison
-                    Result = CompareUsingMostPreciseType(left(), right()) <= 0;
+                    Result = CompareUsingMostPreciseType(left, right) <= 0;
                     break;
 
                 case BinaryExpressionType.Minus:
-                    Result = Numbers.Soustract(left(), right());
+                    Result = Numbers.Soustract(left, right);
                     break;
 
                 case BinaryExpressionType.Modulo:
-                    Result = Numbers.Modulo(left(), right());
+                    Result = Numbers.Modulo(left, right);
                     break;
 
                 case BinaryExpressionType.NotEqual:
                     // Use the type of the left operand to make the comparison
-                    Result = CompareUsingMostPreciseType(left(), right()) != 0;
+                    Result = CompareUsingMostPreciseType(left, right) != 0;
                     break;
 
                 case BinaryExpressionType.Plus:
-                    if (left() is string)
+                    if (left is string)
                     {
-                        Result = String.Concat(left(), right());
+                        Result = String.Concat(left, right);
                     }
                     else
                     {
-                        Result = Numbers.Add(left(), right());
+                        Result = Numbers.Add(left, right);
                     }
 
                     break;
 
                 case BinaryExpressionType.Times:
-                    Result = Numbers.Multiply(left(), right());
+                    Result = Numbers.Multiply(left, right);
                     break;
 
                 case BinaryExpressionType.BitwiseAnd:
-                    Result = Convert.ToUInt16(left()) & Convert.ToUInt16(right());
+                    Result = Convert.ToUInt16(left) & Convert.ToUInt16(right);
                     break;
 
 
                 case BinaryExpressionType.BitwiseOr:
-                    Result = Convert.ToUInt16(left()) | Convert.ToUInt16(right());
+                    Result = Convert.ToUInt16(left) | Convert.ToUInt16(right);
                     break;
 
 
                 case BinaryExpressionType.BitwiseXOr:
-                    Result = Convert.ToUInt16(left()) ^ Convert.ToUInt16(right());
+                    Result = Convert.ToUInt16(left) ^ Convert.ToUInt16(right);
                     break;
 
 
                 case BinaryExpressionType.LeftShift:
-                    Result = Convert.ToUInt16(left()) << Convert.ToUInt16(right());
+                    Result = Convert.ToUInt16(left) << Convert.ToUInt16(right);
                     break;
 
 
                 case BinaryExpressionType.RightShift:
-                    Result = Convert.ToUInt16(left()) >> Convert.ToUInt16(right());
+                    Result = Convert.ToUInt16(left) >> Convert.ToUInt16(right);
                     break;
             }
         }
